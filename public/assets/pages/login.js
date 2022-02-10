91d16f99e1e8ae9802ec1e6fcea5fcef4c2f68fc
adityacaturputra <adityacaturputra25@gmail.com>
2022-02-10 07:20:55 +0700

    authentication page with ajax

---
+++ public/assets/pages/login.js
@@ -0,0 +1,33 @@
+// tunggu hingga dokumen selesai dimuat
+document.addEventListener("DOMContentLoaded", (c) => {
+    
+    
+    const handleSuccess = (msg) => {
+        alert(`selamat datang ${msg.data.first_name} ${msg.data.last_name}`)
+        window.localStorage.setItem('token', msg.data.token)
+        window.location = '/list/order'
+    }
+    
+    const handleError = (req, status, err) => {
+        console.log(req)
+        alert(req.responseJSON.error[0])
+    }
+    
+    $('button#btn-login').on('click', () => {
+        const email = $('input[name=email]').val();
+        const password = $('input[name=password]').val();
+        console.log(email, password)
+        $.ajax({
+            url: '/api/auth',
+            dataType: 'json',
+            method: 'GET',
+            headers: { // kirim header Authorization = base bae64encode (email:password)
+                'Authorization': `basic ${window.btoa(`${email}:${password}`)}`
+            },
+            success: handleSuccess,
+            error: handleError,
+        })
+    })
+})
+
+
---
+++ resources/views/login.blade.php
@@ -0,0 +1,21 @@
+@extends('template')
+
+@section('content')
+    <div class="container d-flex flex-column align-items-center">
+        <div class="card mt-5" style="width: 99%;">
+            <div class="card-body">
+                <h4 class="card-title mb-4">Masuk ke Sistem</h4>
+                <div class="mb-3">
+                    <span class="form-label" id="email">Email</span>
+                    <input type="email" name="email" class="form-control" placeholder="enter your email" aria-label="email" aria-describedby="email">
+                </div>
+                <div class="mb-4">
+                    <span class="form-label" id="password">Password</span>
+                    <input type="password" name="password" class="form-control" placeholder="enter your password" aria-label="password" aria-describedby="password">
+                </div>
+                <button type="button" class="btn btn-primary" id="btn-login">Masuk</button>
+            </div>
+        </div>
+    </div>
+    <script src="assets/pages/login.js"></script>
+@endsection
